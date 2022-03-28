e36d7ea9437c1926d50f80c28fbcb277041391f1
SLMeri <74334648+SLMeri@users.noreply.github.com>
2022-03-28 15:15:43 -0700

    Added JSON handler

--- src/Test.java
+++ src/Test.java
@@ -1,17 +1,6 @@
-import com.players.Character;
-import com.players.npc.Animal;
-import com.players.npc.Friend;
-import com.players.player.Player;
-
 public class Test {
     public static void main(String[] args) {
-        Character aCat = new Animal("Sparky");
-        System.out.println(aCat);
 
-
-
-        Player p1 = new Player("Meri");
-        p1.listCommands();
 
     }
 }
