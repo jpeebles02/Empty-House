16b1c27c83b4f3fd7d9f44d0a61c96db091bc751
Vernon Stephens <97752165+Blackxican93@users.noreply.github.com>
2022-04-06 17:24:57 -0500

    Players can see title menu and start the game, able to see multiple buttons, AB#444

--- src/com/Client/GUI.java
+++ src/com/Client/GUI.java
@@ -4,16 +4,25 @@ import javax.swing.*;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JButton;
 import java.awt.*;
 import java.awt.Font;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GUI {
 
     JFrame frame;
     Container con;
-    JPanel panelName;
-    JLabel labelName;
+    JPanel panelName, startButtonPanel, mainTextPanel, choiceButtonPanel, playerPanel;
+    JLabel labelName, hpLabel, hpLabelNumber, weaponLabel, weaponLabelName;
     Font fontTitle = new Font("Times New Roman", Font.PLAIN, 90);
+    Font normalFont = new Font("Times New Roman", Font.PLAIN, 30);
+    JButton startButton, optionOneButton, optionTwoButton, optionThreeButton, optionFourButton;
+    JTextArea mainTextArea;
+    TitleScreen tScreen = new TitleScreen();
 
     public static void main (String args[]) {
         new GUI();
@@ -21,9 +30,9 @@ public class GUI {
 
     public GUI() {
 
-        frame = new JFrame("Empty_House");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame = new JFrame("Empty House");
         frame.setSize(800,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setBackground(Color.black);
         frame.setLayout(null);
         frame.setVisible(true);
@@ -31,15 +40,125 @@ public class GUI {
 
         panelName = new JPanel();
         panelName.setBounds(100, 100, 600, 150);
-        panelName.setBackground(Color.orange);
+        panelName.setBackground(Color.black);
 
-        labelName = new JLabel("Empty_House");
+        labelName = new JLabel("Empty House");
         labelName.setForeground(Color.white);
         labelName.setFont(fontTitle);
 
+        startButtonPanel = new JPanel();
+        startButtonPanel.setBounds(300, 400, 200, 100);
+        startButtonPanel.setBackground(Color.black);
+
+        startButton = new JButton("START");
+        startButton.setBackground(Color.blue);
+        startButton.setForeground(Color.black);
+        startButton.setFont(normalFont);
+        startButton.addActionListener(tScreen);
+        startButton.setFocusPainted(false);
+
         panelName.add(labelName);
+        startButtonPanel.add(startButton);
+
         con.add(panelName);
+        con.add(startButtonPanel);
         //Creating the MenuBar and adding components
+    }
+
+    public void GameScreen() {
+
+        panelName.setVisible(false);
+        startButtonPanel.setVisible(false);
+
+        mainTextPanel = new JPanel();
+        mainTextPanel.setBounds(100, 100, 600, 250);
+        mainTextPanel.setBackground(Color.black);
+        con.add(mainTextPanel);
+
+        mainTextArea = new JTextArea("Welcome to Empty House!");
+        mainTextArea.setBounds(100, 100, 600, 250);
+        mainTextArea.setBackground(Color.black);
+        mainTextArea.setForeground(Color.white);
+        mainTextArea.setFont(normalFont);
+        mainTextArea.setLineWrap(true);
+        mainTextPanel.add(mainTextArea);
+
+        choiceButtonPanel = new JPanel();
+        choiceButtonPanel.setBounds(250, 350, 300, 150);
+        choiceButtonPanel.setBackground(Color.black);
+        choiceButtonPanel.setLayout(new GridLayout(4,1));
+        con.add(choiceButtonPanel);
+
+        optionOneButton = new JButton("Option 1");
+        optionOneButton.setBackground(Color.black);
+        optionOneButton.setForeground(Color.black);
+        optionOneButton.setFont(normalFont);
+        optionOneButton.setFocusPainted(false);
+        choiceButtonPanel.add(optionOneButton);
+
+
+        optionTwoButton = new JButton("Option 2");
+        optionTwoButton.setBackground(Color.black);
+        optionTwoButton.setForeground(Color.black);
+        optionTwoButton.setFont(normalFont);
+        optionTwoButton.setFocusPainted(false);
+        choiceButtonPanel.add(optionTwoButton);
+
+        optionThreeButton = new JButton("Option 3");
+        optionThreeButton.setBackground(Color.black);
+        optionThreeButton.setForeground(Color.black);
+        optionThreeButton.setFont(normalFont);
+        optionThreeButton.setFocusPainted(false);
+        choiceButtonPanel.add(optionThreeButton);
+
+        optionFourButton = new JButton("Option 4");
+        optionFourButton.setBackground(Color.black);
+        optionFourButton.setForeground(Color.black);
+        optionFourButton.setFont(normalFont);
+        optionFourButton.setFocusPainted(false);
+        choiceButtonPanel.add(optionFourButton);
+
+        playerPanel = new JPanel();
+        playerPanel.setBounds(100, 15, 600, 50);
+        playerPanel.setBackground(Color.black);
+        playerPanel.setLayout(new GridLayout(1, 4));
+        con.add(playerPanel);
+
+        hpLabel = new JLabel("HP:");
+        hpLabel.setFont(normalFont);
+        hpLabel.setForeground(Color.white);
+        playerPanel.add(hpLabel);
+
+        hpLabelNumber = new JLabel();
+        hpLabelNumber.setFont(normalFont);
+        hpLabelNumber.setForeground(Color.white);
+        playerPanel.add(hpLabelNumber);
+
+        weaponLabel = new JLabel("Weapon:");
+        weaponLabel.setFont(normalFont);
+        weaponLabel.setForeground(Color.white);
+        playerPanel.add(weaponLabel);
+
+        weaponLabelName = new JLabel();
+        weaponLabelName.setFont(normalFont);
+        weaponLabelName.setForeground(Color.white);
+        playerPanel.add(weaponLabelName);
+
+        playerSetup();
 
     }
+
+    public void playerSetup(){
+        int playerHP = 20;
+        String weapon = "Sword";
+        weaponLabelName.setText(weapon);
+        hpLabelNumber.setText("" + playerHP);
+    }
+
+    public class TitleScreen implements ActionListener {
+        public void actionPerformed(ActionEvent event){
+            GameScreen();
+        }
+    }
+
 }
