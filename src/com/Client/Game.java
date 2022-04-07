77b9150c804f65195cf145bb5fc7e5798114e14e
Vernon Stephens <97752165+Blackxican93@users.noreply.github.com>
2022-04-07 12:18:28 -0500

    Modified locations.json and added in option to add location to game, AB#575

--- src/com/Client/Game.java
+++ src/com/Client/Game.java
@@ -5,14 +5,17 @@ import com.Util.Dir;
 import com.House.Room;
 import com.gameobjects.*;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 import com.Client.*;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 
 public class Game implements java.io.Serializable {
@@ -238,6 +241,131 @@ public class Game implements java.io.Serializable {
             }
         }
         return s;
+    }
+
+    void AddItems () {
+        String input = "";
+        String itemNameOption = "";
+        String itemDescriptionOption = "";
+        String itemAttributeOption = "";
+        String itemLocationOption = "";
+
+        JSONParser jsonParser = new JSONParser();
+
+
+        JSONObject addItems = new JSONObject();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Would you like to add an item to the game? Type Y or N ");
+
+        input = in.nextLine();
+        System.out.println("You have entered: " + input);
+
+        if (input.equals("Y")) {
+            try {
+                Object obj = jsonParser.parse(new FileReader("item.json"));
+                JSONArray jsonArray = (JSONArray) obj;
+
+                System.out.println(jsonArray);
+
+                System.out.println("You have the option of creating an item. Simply type in your command name using one word. ");
+                itemNameOption = in.nextLine();
+                System.out.println("You have entered: " + itemNameOption);
+                addItems.put("itemName", itemNameOption);
+
+                System.out.println("You have the option of adding in an item description. Simply type in your command description phrase. ");
+                itemDescriptionOption = in.nextLine();
+                System.out.println("You have entered: " + itemDescriptionOption);
+                addItems.put("itemDescription", itemDescriptionOption);
+
+                System.out.println("You have the option of adding in an item attribute. Simply type in your command example. ");
+                itemAttributeOption = in.nextLine();
+                System.out.println("You have entered: " + itemAttributeOption);
+                addItems.put("itemAttribute", itemAttributeOption);
+
+                System.out.println("You have the option of adding in an item location. Simply type in your command example. ");
+                itemLocationOption = in.nextLine();
+                System.out.println("You have entered: " + itemLocationOption);
+                addItems.put("itemLocation", itemLocationOption);
+
+                jsonArray.add(addItems);
+                System.out.println("Your options have been added to the list ");
+                System.out.println(jsonArray);
+
+                FileWriter file = new FileWriter("item.json");
+                file.write(jsonArray.toJSONString());
+                file.flush();
+                file.close();
+
+            } catch(ParseException | IOException e){
+                e.printStackTrace();
+            }
+
+        }
+        else if (input.equals("N")) {
+            System.out.println("No additions were made. ");
+        } else {
+            System.out.println("Invalid command. ");
+        }
+    }
+
+    void AddLocations(){
+        String input = "";
+        String locationNameOption = "";
+        String locationDescriptionOption = "";
+        String locationItemsOption = "";
+        String locationDirectionsOption = "";
+
+        JSONParser jsonParser = new JSONParser();
+
+        JSONObject addLocation = new JSONObject();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Would you like to add a location to the game? Type Y or N ");
+
+        input = sc.nextLine();
+        System.out.println("You have entered: " + input);
+
+        if (input.equals("Y")) {
+            try {
+                Object obj = jsonParser.parse(new FileReader("locations.json"));
+                JSONArray jsonArray = (JSONArray)obj;
+
+                System.out.println(jsonArray);
+
+
+                System.out.println("You have the option of creating a location inside of the house. Simply type in your location name using one word. ");
+                locationNameOption = sc.nextLine();
+                System.out.println("You have entered: " + locationNameOption);
+                addLocation.put("room_name", locationNameOption);
+
+                System.out.println("You have the option of creating a location direction inside of the house. Simply type in your location direction. ");
+                locationDirectionsOption = sc.nextLine();
+                System.out.println("You have entered: " + locationDirectionsOption);
+                addLocation.put("directions", locationDirectionsOption);
+
+                System.out.println("You have the option of creating a location description. Simply type in your location description. ");
+                locationDescriptionOption = sc.nextLine();
+                System.out.println("You have entered: " + locationDescriptionOption);
+                addLocation.put("description", locationDescriptionOption);
+
+
+                jsonArray.add(addLocation);
+                System.out.println("Your options have been added to the list ");
+                System.out.println(jsonArray);
+
+                FileWriter file = new FileWriter("locations.json");
+                file.write(jsonArray.toJSONString());
+                file.flush();
+                file.close();
+
+            } catch (ParseException | IOException e) {
+                e.printStackTrace();
+            }
+        } else if (input.equals("N")) {
+            System.out.println("No additions were made. ");
+        } else {
+            System.out.println("Invalid command. ");
+        }
+
     }
 
 
